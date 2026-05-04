41542c6a355f6b3bd9185a9caa90995339973965
Rindy Portfolio <rindy@tadpole.cc>
2026-05-04 14:39:09 -0400

    WIP: latest plan updates

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 // metro.config.js
 const { getDefaultConfig } = require('@expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
@@ -12,5 +13,21 @@ config.resolver.extraNodeModules = {
 // Add resolver configuration for ES modules compatibility
 config.resolver.unstable_enablePackageExports = false;
 config.resolver.unstable_conditionNames = ['react-native', 'browser', 'require'];
+
+// posthog-react-native 4.44+ uses @posthog/core/surveys subpath exports which
+// Metro can't resolve when unstable_enablePackageExports is false. resolveRequest
+// intercepts from any origin (including within node_modules, unlike extraNodeModules).
+const originalResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === '@posthog/core/surveys') {
+    return {
+      filePath: path.resolve(__dirname, 'node_modules/@posthog/core/dist/surveys/index.js'),
+      type: 'sourceFile',
+    };
+  }
+  return originalResolveRequest
+    ? originalResolveRequest(context, moduleName, platform)
+    : context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
